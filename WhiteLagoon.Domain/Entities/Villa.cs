aa8eafdaaf09184d851d304b95687467914f1bd2
Miles Norman <Miles Norman@DESKTOP-2302M23>
2025-02-25 19:31:38 +0000

    IFormField added (file upload - for image url on villa entity). In the villa controller create view, I've added the input for just Image and hidden the imageurl. In the form method in the create view I've added enctype which is needed to enable to file upload to work. will error/fail without this! is a must to remember

--- WhiteLagoon.Domain/Entities/Villa.cs
+++ WhiteLagoon.Domain/Entities/Villa.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Http; //needed to use IFormFile
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
@@ -30,6 +31,8 @@ namespace WhiteLagoon.Domain.Entities
 
         //The below is a data annotation. ImageUrl will now show as Image Url
         //When typing the below code, right click Display and add System
+        [NotMapped] //this is telling the app not to add the IFormFile below to the database
+        public IFormFile? Image { get; set; } //Need the using statement of aspnetcore http at the very top to work with IFormFile
         [Display(Name= "Image Url")]
         public string? ImageUrl { get; set; }
         public DateTime? Created_Date { get; set; }
